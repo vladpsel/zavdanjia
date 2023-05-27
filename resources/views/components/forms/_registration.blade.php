300e22ef66cad65c933d56249d0393bf8fa06f4a
vladpsel <vpanovwr@gmail.com>
2023-05-27 22:34:16 +0300

    update(UI): update registration form

--- resources/views/components/forms/_registration.blade.php
+++ resources/views/components/forms/_registration.blade.php
@@ -1,21 +1,21 @@
 <form action="#" class="user-form" method="post">
     @csrf
     <fieldset>
-        <p class="label">Email</p>
+        <p class="label">Email:</p>
         <label>
             <input type="email" name="email" value="" placeholder="" required>
         </label>
     </fieldset>
 
     <fieldset>
-        <p class="label">Пароль</p>
+        <p class="label">Пароль:</p>
         <label>
             <input type="password" name="password" value="" placeholder="" required>
         </label>
     </fieldset>
 
     <fieldset>
-        <p class="label">Повторіть пароль</p>
+        <p class="label">Повторіть пароль:</p>
         <label>
             <input type="password" name="password_confirmation" value="" placeholder="" required>
         </label>
@@ -26,6 +26,21 @@
             Зареєструватися
         </button>
     </fieldset>
+
+    @if (Route::has('login'))
+        <div class="note mb-1">
+            <span>Вже зареєстровані?</span>
+            <a href="{{ route('login') }}" class="colored">Увійдіть в аккаунт</a>
+        </div>
+    @endif
+
+    @if ($errors->any())
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li class="error">{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
 
 
 </form>
